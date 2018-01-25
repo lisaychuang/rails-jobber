64e73ceb90e8fd0b71760c868662c1bcc259affd
Lisa Huang <lisa.yc.huang@gmail.com>
2018-01-25 15:03:18 -0800

    Added index, show, edit, update actions to UserController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,32 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-
   def new
   end
 
   def show
+    @user = User.find_by(id: params[:id])
   end
 
   def edit
+    @user = User.find_by(id: params[:id])
   end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    binding.pry
+    if @user.update(user_params)
+        redirect_to user_path(@user)
+    else
+        redirect_to edit_user_path
+    end
+  end
+
+  private 
+    
+    
+  def user_params
+      params.require(:user).permit(:first_name, :last_name, :email)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
     has_many :applications
     has_many :work_experiences 
     
+    def full_name
+        self.first_name + ' ' + self.last_name 
+    end
 end
