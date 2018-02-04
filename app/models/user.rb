241585fbc60b5b8ff39ccac9bbbc89e198d1f7ba
Lisa Huang <lisa.yc.huang@gmail.com>
2018-02-03 20:16:10 -0800

    Added validation rules for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 require 'bcrypt'
 
 class User < ActiveRecord::Base
+    before_save { self.email = email.downcase }
+    validates :name,  presence: true, length: { maximum: 50 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+    validates :password, presence: true
+
     include BCrypt
     has_secure_password
     has_many :applications
