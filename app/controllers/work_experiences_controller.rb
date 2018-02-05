e2be458c8db0583abcfd3e85874e05cb4cfe6778
Lisa Huang <lisa.yc.huang@gmail.com>
2018-02-04 17:54:51 -0800

    Added edit view for nested WorkExperience, and both #edit #update actions in WorkExperiences controller

--- app/controllers/work_experiences_controller.rb
+++ app/controllers/work_experiences_controller.rb
@@ -7,29 +7,45 @@ class WorkExperiencesController < ApplicationController
   end
 
   def new
+    if params[:user_id] && !User.exists?(params[:user_id])
+      redirect_to users_path, alert: "User not found."
+    else
+      @experience = WorkExperience.new(user_id: params[:user_id])
+    end
   end
 
   def show
     @experience = WorkExperience.find_by(id: params[:id])
   end
-
+  
   def edit
-    @experience = WorkExperience.find_by(id: params[:id])
+    if params[:user_id]
+      @user = User.find_by(id: params[:user_id])
+      if @user.nil?
+        redirect_to users_path, alert: "User not found."
+      else
+        @experience = @user.work_experiences.find_by(id: params[:id])
+        redirect_to user_work_experiences_path(@user), alert: "Work experience not found." if @experience.nil?
+      end
+    else
+      @experience = WorkExperience.find(params[:id])
+    end
   end
 
   def update
-    @experience = WorkExperience.find_by(id: params[:id])
+    @user = User.find_by(id: params[:user_id])
+    @experience = @user.work_experiences.find_by(id: params[:id])
     if @experience.update(experience_params)
-        redirect_to user_path(@experience)
+        redirect_to user_work_experience_path(@user, @experience)
     else
-        redirect_to edit_user_path
+        redirect_to edit_user_work_experience_path(@user, @experience)
     end
   end
 
   private 
     
   
-  def user_params
-      params.require(:user).permit(:first_name, :last_name, :email)
+  def experience_params
+      params.require(:work_experience).permit(:employer, :title, :location, :start_date, :end_date, :experience, :ranking)
   end
 end
